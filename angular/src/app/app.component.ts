ace2ea11c7927788f0ac4435f4fb67cb1a036146
Pavel <Pavel.Shikhov@devexpress.com>
2020-11-13 11:13:59 +0300

    Minor fixes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -9,12 +9,12 @@ import {Appointment, Priority, AppService} from './app.service';
   providers: [AppService]
 })
 export class AppComponent {
-    appointmentsData: Appointment[];
-    prioritiesData: Priority[];
+    data: Appointment[];
+    priorities: Priority[];
     currentDate: Date = new Date(2021, 4, 25);
 
     constructor(service: AppService) {
-      this.appointmentsData = service.getAppointments();
-      this.prioritiesData = service.getPriorities();
+      this.data = service.getAppointments();
+      this.priorities = service.getPriorities();
     }
 }
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -61,10 +61,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
 
-
-<style>
 #scheduler {
   height: 600px;
 }
